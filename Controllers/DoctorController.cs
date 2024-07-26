c9b7580859686a8f5b7e5a43f0eb773b707ba232
unknown <git config --global user.name 123mohamedase>
2024-07-26 13:35:26 -0700

    Add initial project files

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,25 @@
+﻿using Clinics_Center.Data;
+using Clinics_Center.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clinics_Center.Controllers
+{
+    public class DoctorController : Controller
+    {
+        ApplicationDbContext context= new ApplicationDbContext();
+        public IActionResult Bookappointment()
+        {
+            var result = context.Doctors.ToList();
+            return View(result);
+        }
+
+        public IActionResult getBookappointment(int Id)
+        {
+            var result = context.Doctors.Find(Id);
+            return View(result);
+        }
+
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Clinics_Center.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
